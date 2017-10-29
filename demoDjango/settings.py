f9b888a2c1c414ce2a7e31172fed293a50cd9fa4
Joseph Ashwin <ashwinkjoseph@gmail.com>
2017-10-29 09:51:54 +0530

    Getting ready for heroku

--- demoDjango/settings.py
+++ demoDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0c_v^0sq)-6h1%^zue(^54=#oz@6qmm5hd76il@*gr*7!la-)*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangokochipython.herokuapp.com']
 
 
 # Application definition
