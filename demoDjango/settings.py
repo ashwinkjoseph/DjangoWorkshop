30e6c4b07ee3b98754b1a4d8425e4de56c549986
Joseph Ashwin <ashwinkjoseph@gmail.com>
2017-10-29 09:49:47 +0530

    Getting ready for heroku

--- demoDjango/settings.py
+++ demoDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0c_v^0sq)-6h1%^zue(^54=#oz@6qmm5hd76il@*gr*7!la-)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
